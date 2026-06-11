b985870364dc7469656221ef2f1ca2e05d2b1638
Anand Vivek <anand1234.vivek@gmail.com>
2026-06-11 15:07:23 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -103,7 +103,7 @@ export default defineConfig({
        
         ],
 
-        navigateFallback: "/offline.html",
+        navigateFallback: "/index.html",
         navigateFallbackDenylist: [/^\/api/],
 
         runtimeCaching: [
