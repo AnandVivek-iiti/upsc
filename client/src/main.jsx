597178842860a60892d6920430fcb4966377052f
Anand Vivek <anand1234.vivek@gmail.com>
2026-06-26 10:27:48 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,29 +1,26 @@
+// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { registerSW } from "virtual:pwa-register";
 import PWAInstallPrompt from "./components/PWAInstallPrompt";
-let isApplyingUpdate = false;
 
 const updateSW = registerSW({
   onNeedRefresh() {
-    if (isApplyingUpdate) return;
-    isApplyingUpdate = true;
-    updateSW(true);
+    console.log("[PWA] Update available.");
   },
   onOfflineReady() {
     console.log("[PWA] App ready for offline use.");
   },
 });
-window.__pwaPrompt = null;
 
+window.__pwaPrompt = null;
 window.addEventListener("beforeinstallprompt", (e) => {
   e.preventDefault();
   window.__pwaPrompt = e;
 });
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App /> 
 );
